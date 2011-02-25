db9a94c8379df5f20d2c185e487d65c4c1ccd7c9
samo9789 <samantha.halfon@gmail.com>
2011-02-25 21:50:51 +0100

    TotalSeconds changed to TotalMilliseconds against replay attack issues when calling the service often quickly

--- OAuthRequest.cs
+++ OAuthRequest.cs
@@ -35,7 +35,7 @@ namespace OAuth
         private string _generateTimestamp()
         {
             TimeSpan ts = DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, 0);
-            return Convert.ToInt64(ts.TotalSeconds).ToString();
+            return Convert.ToInt64(ts.TotalMilliseconds).ToString();
         }
 
         /// <summary>
